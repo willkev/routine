f7d692211ea78d4ff2ce3ad4a3465262686a460d
WillianKirschner <willkev@gmail.com>
2014-11-26 09:32:29 -0200

    Pacote "especial" com classes não relacionadas ao projeto, apenas testes experimentais!

---
+++ routine/src/Others/FinderMS.java
@@ -0,0 +1,55 @@
+package Others;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FinderMS {
+
+//    public static void main(String[] args) throws Exception {
+//        //new FinderMS(",7,16,33,42,50,58,");
+//        new FinderMS();
+//    }
+
+    public FinderMS(String toFind) throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get(new File("src").getAbsolutePath(), "ms.txt"));
+        int max = 1;
+        for (String line : lines) {
+            int count = 0;
+            int i = 0;
+            for (String number : line.split(",")) {
+                if (i >= 2) {
+                    if (toFind.contains("," + number + ",")) {
+                        count++;
+                    }
+                }
+                i++;
+            }
+            if (count >= max) {
+                max = count;
+                System.out.println("[" + max + "] " + line);
+            }
+        }
+    }
+
+    public FinderMS() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(new File("src").getAbsolutePath(), "ms.txt"));
+        int[] numeros = new int[61];
+        for (String line : lines) {
+            int i = 0;
+            int num;
+            for (String number : line.split(",")) {
+                if (i >= 2) {
+                    num = Integer.parseInt(number);
+                    numeros[num]++;
+                }
+                i++;
+            }
+        }
+        for (int x = 1; x < numeros.length; x++) {
+            System.out.format("[%s]\t%s\n", x, numeros[x]);
+        }
+    }
+}
---
+++ routine/src/Others/Perceptron2.java
@@ -0,0 +1,61 @@
+package Others;
+
+import java.util.Random;
+
+public class Perceptron2 {
+
+    // The Perceptron2 stores its weights and learning constants.
+    float[] weights;
+    float c = 0.01F;
+
+    public Perceptron2(int n) {
+        weights = new float[n];
+        // Weights start off random.
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = random(-1, 1);
+            System.out.println("weights[" + i + "] = " + weights[i]);
+        }
+    }
+
+    // Return an output based on inputs.
+    public int feedforward(float[] inputs) {
+        float sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += inputs[i] * weights[i];
+        }
+        System.out.println("sum = " + sum);
+        return activate(sum);
+    }
+
+    // Output is a +1 or -1.
+    private int activate(float sum) {
+        if (sum > 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    // Train the network against known data.
+    private void train(float[] inputs, int desired) {
+        int guess = feedforward(inputs);
+        float error = desired - guess;
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] += c * error * inputs[i];
+        }
+    }
+
+    private float random(int ini, int end) {
+        Random r = new Random();
+        return r.nextFloat();
+    }
+    
+//    public static void main(String[] args) {
+//        Perceptron2 p = new Perceptron2(3);
+//        // The input is 3 values: x,y and bias.
+//        float[] point = {50, -12, 1};
+//        // The answer!
+//        int result = p.feedforward(point);
+//        System.out.println("result = " + (result > 1 ? true : false));        
+//    }
+}
