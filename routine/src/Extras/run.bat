b336c838ea52eaa6acc162366f6d76f3b1deeb2d
WillianKirschner <willkev@gmail.com>
2014-11-26 09:24:51 -0200

    Recebe parâmetros para executar log e/ou salvar em arquivo.

--- routine/src/Extras/run.bat
+++ routine/src/Extras/run.bat
@@ -1,8 +1,9 @@
 @echo off 
+title routine
 java -classpath routine.jar;JNativeHook.jar core.Executor
 if %errorlevel%  neq 0 (
 	echo.
-	echo Info: routine.jar e JNativeHook.jar devem estar na mesma pasta!
+	echo Info: routine.jar e JNativeHook.jar devem estar na mesma pasta deste Batch!
         echo.
         echo.
 	pause
