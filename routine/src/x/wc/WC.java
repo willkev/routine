2f1a9dfe421e7e49a09566261c0c197f4fb9e4c4
willian <willian@animati.com.br>
2015-03-26 19:38:50 -0300

    WC add!

---
+++ routine/src/x/wc/WC.java
@@ -0,0 +1,29 @@
+//http://webcam-capture.sarxos.pl/
+package x.wc;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import javax.swing.JFrame;
+
+public class WC {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+        WebcamPanel panel = new WebcamPanel(webcam);
+        panel.setFPSDisplayed(true);
+        panel.setDisplayDebugInfo(true);
+        panel.setImageSizeDisplayed(true);
+        panel.setMirrored(true);
+
+        JFrame window = new JFrame("Test webcam panel");
+        window.add(panel);
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+}
